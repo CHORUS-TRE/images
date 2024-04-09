d2b854c10719317261573b6f366c88d7054b5e5b
Nathalie Casati <nathalie.casati@chuv.ch>
2024-04-09 15:41:22 +0200

    image now pushes to our registry

--- apps/brainstorm/build.sh
+++ apps/brainstorm/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 docker build -t registry.dip-dev.thehip.app/brainstorm:latest --build-arg APP_NAME=brainstorm --build-arg VERSION=R2022b --build-arg UPDATE=7 .
+docker tag registry.dip-dev.thehip.app/brainstorm:latest registry.build.chorus-tre.ch/brainstorm:latest
+docker push registry.build.chorus-tre.ch/brainstorm:latest
