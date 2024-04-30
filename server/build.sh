a78b7e8b4586e489446d8310c4b849f110d4dbbb
Nathalie Casati <nathalie.casati@chuv.ch>
2024-04-30 11:50:14 +0200

    modified build script and added server

--- server/build.sh
+++ server/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 VERSION=0.0.1
-APP_NAME="brainstorm"
+APP_NAME="xpra-server"
 APP_VERSION="latest"
 REGISTRY="registry.build.chorus-tre.ch"
 
-docker build -t ${REGISTRY}/${APP_NAME}:${VERSION} --build-arg APP_NAME=${APP_NAME} --build-arg APP_VERSION=${APP_VERSION} --build-arg MAT_VERSION=R2022b --build-arg MAT_UPDATE=7 .
+docker build -t ${REGISTRY}/${APP_NAME}:${VERSION} --build-arg APP_NAME=${APP_NAME} --build-arg APP_VERSION=${APP_VERSION} .
 docker tag ${REGISTRY}/${APP_NAME}:${VERSION} ${REGISTRY}/${APP_NAME}:latest
 docker push ${REGISTRY}/${APP_NAME}:latest
 docker push ${REGISTRY}/${APP_NAME}:$VERSION
