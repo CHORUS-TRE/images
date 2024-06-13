84923fa0f1d7d03c602bcfae6da87c4024d69f53
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-06-13 16:57:16 +0200

    only spaces no tab, change name, create symlink for Dockerfile

--- server/build.sh
+++ server/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 VERSION=0.0.1
-APP_NAME="xpra"
+APP_NAME="xpra-server"
 APP_VERSION="latest"
 REGISTRY="registry.build.chorus-tre.local"
 
@@ -24,14 +24,13 @@ OUTPUT="type=${OUTPUT:-docker}"
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
 exec docker buildx build \
-	--pull \
-    -f "Dockerfile.ubuntu-main" \
+    --pull \
     -t ${REGISTRY}/${APP_NAME} \
     -t ${REGISTRY}/${APP_NAME}:${VERSION} \
-	--label "APP_NAME=${APP_NAME}" \
-	--label "APP_VERSION=${APP_VERSION}" \
-	--build-arg "APP_NAME=${APP_NAME}" \
-	--build-arg "APP_VERSION=${APP_VERSION}" \
+    --label "APP_NAME=${APP_NAME}" \
+    --label "APP_VERSION=${APP_VERSION}" \
+    --build-arg "APP_NAME=${APP_NAME}" \
+    --build-arg "APP_VERSION=${APP_VERSION}" \
     --build-arg "XPRA_VERSION=${XPRA_VERSION}" \
     --build-arg "XPRA_KEYCLOAK_AUTH=${XPRA_KEYCLOAK_AUTH}" \
     --build-arg "XPRA_KEYCLOAK_SERVER_URL=${XPRA_KEYCLOAK_SERVER_URL}" \
@@ -44,5 +43,5 @@ exec docker buildx build \
     --build-arg "XPRA_KEYCLOAK_AUTH_GROUPS=${XPRA_KEYCLOAK_AUTH_GROUPS}" \
     --build-arg "XPRA_KEYCLOAK_AUTH_CONDITION=${XPRA_KEYCLOAK_AUTH_CONDITION}" \
     --build-arg "XPRA_KEYCLOAK_GRANT_TYPE=${XPRA_KEYCLOAK_GRANT_TYPE}" \
-	--output=$OUTPUT \
-	.
+    --output=$OUTPUT \
+    .
