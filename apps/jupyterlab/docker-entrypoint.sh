1e11543cbf404b598f9554b91d3807030310faff
Yoan Blanc <yoan.blanc@chuv.ch>
2024-07-02 11:31:00 +0200

    fixup! feat(jupyterlab): new app
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>

--- apps/jupyterlab/docker-entrypoint.sh
+++ apps/jupyterlab/docker-entrypoint.sh
@@ -2,11 +2,6 @@
 
 HIP_USER=hip
 
-# Starting a DBUS
-dbus-uuidgen > /var/lib/dbus/machine-id
-mkdir -p /var/run/dbus
-DBUS_SESSION_BUS_ADDRESS="$(dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address)"
-
 APP_CMD_PREFIX="export DISPLAY=${DISPLAY}; ${APP_CMD_PREFIX}"
 APP_CMD_PREFIX="export DBUS_SESSION_BUS_ADDRESS=${DBUS_SESSION_BUS_ADDRESS}; ${APP_CMD_PREFIX}"
 CMD="$APP_CMD_PREFIX; $APP_CMD"
