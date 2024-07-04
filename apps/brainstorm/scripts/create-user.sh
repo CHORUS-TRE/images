430d4cf98ad0eb21b185f924a2903bd827b22cff
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-07-04 10:15:17 +0200

    Bump brainstorm to ubuntu 24

--- apps/brainstorm/scripts/create-user.sh
+++ apps/brainstorm/scripts/create-user.sh
@@ -26,7 +26,7 @@ if [ $? -eq 0 ]; then
   echo "$HIP_USER already exists."
   exit 0
 else
-  useradd --create-home --shell /bin/bash $HIP_USER --uid 1000
+  useradd --create-home --shell /bin/bash $HIP_USER --uid 1001
   if [ $? -eq 0 ]; then
     echo "done."
   else
