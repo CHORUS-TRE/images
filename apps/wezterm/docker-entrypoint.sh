1b767257276f989588c6336a9bfe60e3052d2047
Yoan Blanc <yoan.blanc@chuv.ch>
2024-07-22 10:03:39 +0200

    fixup! feat(wezterm): sample app
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>

--- apps/wezterm/docker-entrypoint.sh
+++ apps/wezterm/docker-entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -xeu
 
-# Fix permissions on the volume
-chmod -R 1777 /tmp/.X11-unix
-
-# Pass some environment variables down.
-ENV="XPRA_PASSWORD=${XPRA_PASSWORD}"
-
 # Perform any command changes here (at your own risk)
 CMD="$@"
 
