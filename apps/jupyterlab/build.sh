df1b8b04cb6fa912a9411d5e1932c714d0625077
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-08-16 19:04:12 +0200

    chorusification

--- apps/jupyterlab/build.sh
+++ apps/jupyterlab/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="0.0.1"
+VERSION="4.2.1-1"
 APP_NAME="jupyterlab"
 # https://github.com/jupyterlab/jupyterlab-desktop/releases
 APP_VERSION="4.2.1-1"
@@ -15,7 +15,8 @@ OUTPUT="type=${OUTPUT:-docker}"
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
-exec docker buildx build \
+cp -r ../../core ./core
+docker buildx build \
     --pull \
     -t ${REGISTRY}/${APP_NAME} \
     -t ${REGISTRY}/${APP_NAME}:${VERSION} \
@@ -26,3 +27,4 @@ exec docker buildx build \
     --build-arg "MINIFORGE3_VERSION=${MINIFORGE3_VERSION}" \
     --output=$OUTPUT \
     .
+rm -rf core
