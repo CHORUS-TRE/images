43f8d8eafd3a8457ecadcca61dd799aae4e19525
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-08-19 14:37:31 +0200

    chorusify brainstorm

--- apps/brainstorm/build.sh
+++ apps/brainstorm/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=0.0.2
+VERSION=0.0.3
 APP_NAME="brainstorm"
 APP_VERSION="latest"
 REGISTRY="${REGISTRY:=registry.build.chorus-tre.local}"
@@ -10,7 +10,8 @@ OUTPUT="type=${OUTPUT:-docker}"
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
-exec docker buildx build \
+cp -r ../../core ./core
+docker buildx build \
     --pull \
     -t ${REGISTRY}/${APP_NAME} \
     -t ${REGISTRY}/${APP_NAME}:${VERSION} \
@@ -22,3 +23,4 @@ exec docker buildx build \
     --build-arg "MAT_UPDATE=6" \
     --output=$OUTPUT \
     .
+rm -rf core
