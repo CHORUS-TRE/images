44363041c2cddaa04706e1ca858062b344f89a12
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-08-23 16:45:46 +0200

    Get the dir of the script at runtime to launch it from wherever (#35)
    shellcheck everything

--- core/utilities/chorus-utils.sh
+++ core/utilities/chorus-utils.sh
@@ -10,8 +10,9 @@ apt-get -qq update && \
 apt-get -qq upgrade -y && \
 apt-get install -qq --no-install-recommends -y curl ca-certificates gnupg && \
 
-./install-vgl.sh "3.1.1-20240228"
-./install-utility-tools.sh "$@"
+script_dir=$(dirname "$0")
+"$script_dir"/install-vgl.sh "3.1.1-20240228"
+"$script_dir"/install-utility-tools.sh "$@"
 echo "===> Creating directory /docker-entrypoint.d"
 mkdir /docker-entrypoint.d
 
