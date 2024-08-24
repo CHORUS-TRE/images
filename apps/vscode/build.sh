76ac093ad4b2325e6197f9e74d418dd8405e2de2
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-08-24 17:38:54 +0200

    feat(vscode) : new app

---
+++ apps/vscode/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+APP_NAME="vscode"
+APP_VERSION=1.92.2-1723660989
+PKG_REL="1"
+
+# If the APP_VERSION is bumped, reset the PKG_REL
+# otherwhise, please bump the PKG_REL on any changes.
+VERSION="${APP_VERSION}-${PKG_REL}"
+
+REGISTRY="${REGISTRY:=registry.build.chorus-tre.local}"
+
+# Use `registry` to build and push
+OUTPUT="type=${OUTPUT:-docker}"
+
+# Tip: use `BUILDKIT_PROGRESS=plain` to see more.
+
+cp -r ../../core ./core
+docker buildx build \
+    --pull \
+    -t ${REGISTRY}/${APP_NAME} \
+    -t ${REGISTRY}/${APP_NAME}:${VERSION} \
+    --label "APP_NAME=${APP_NAME}" \
+    --label "APP_VERSION=${APP_VERSION}" \
+    --build-arg "APP_NAME=${APP_NAME}" \
+    --build-arg "APP_VERSION=${APP_VERSION}" \
+    --output=$OUTPUT \
+    .
+rm -rf core
