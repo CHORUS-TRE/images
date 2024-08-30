3b14a0a162fd75ffa30e9e56621f78ae7abb54cf
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-08-30 15:37:40 +0200

    add config files , needed for specific apps config (fsl for instance)

---
+++ core/entrypoint/2-copy-config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$CHORUS_USER" ]; then
+  echo "CHORUS_USER is not set. Exiting."
+  exit 1
+fi
+
+if [ -z "$CONFIG_ARRAY" ]; then
+  echo "CONFIG_ARRAY is not set. Exiting."
+  exit 0
+fi
+
+# Convert CONFIG_ARRAY string to an array
+CONFIG_ARRAY=( "$CONFIG_ARRAY" )
+
+#copy all configuration files in $CONFIG_ARRAY in $CHORUS_USER homedir
+for CONFIG in "${CONFIG_ARRAY[@]}"; do
+  echo -n "Copying ${CONFIG} to ${CHORUS_USER} homedir... "
+  cp -r /apps/"${APP_NAME}"/config/"${CONFIG}" /home/"${CHORUS_USER}"
+  chown -R "${CHORUS_USER}":"${CHORUS_USER}" /home/"${CHORUS_USER}"/"${CONFIG}"
+  echo "Configuration copied for ${CHORUS_USER}"
+done
