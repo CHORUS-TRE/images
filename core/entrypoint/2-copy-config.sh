ee51000ba57888807c8415ddbd22111b939f1a2e
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-09-02 12:58:17 +0200

    Update core/entrypoint/2-copy-config.sh
    Co-authored-by: Yoan Blanc <yoan.blanc@chuv.ch>

--- core/entrypoint/2-copy-config.sh
+++ core/entrypoint/2-copy-config.sh
@@ -16,7 +16,7 @@ CONFIG_ARRAY=( "$CONFIG_ARRAY" )
 #copy all configuration files in $CONFIG_ARRAY in $CHORUS_USER homedir
 for CONFIG in "${CONFIG_ARRAY[@]}"; do
   echo -n "Copying ${CONFIG} to ${CHORUS_USER} homedir... "
-  cp -r /apps/"${APP_NAME}"/config/"${CONFIG}" /home/"${CHORUS_USER}"
-  chown -R "${CHORUS_USER}":"${CHORUS_USER}" /home/"${CHORUS_USER}"/"${CONFIG}"
+  cp -r /apps/"${APP_NAME}"/config/"${CONFIG}" ~"${CHORUS_USER}"
+  chown -R "${CHORUS_USER}": ~"${CHORUS_USER}"/"${CONFIG}"
   echo "Configuration copied for ${CHORUS_USER}"
 done
