ff9ebfaa14236804cd3ecf698baaca972d4deba4
Yoan Blanc <yoan.blanc@chuv.ch>
2024-09-06 15:12:19 +0200

    chore: bump to Ubuntu 24.04
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>

--- apps/jupyterlab/build.sh
+++ apps/jupyterlab/build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-VERSION="4.2.1-1"
+set -e
+
 APP_NAME="jupyterlab"
 # https://github.com/jupyterlab/jupyterlab-desktop/releases
-APP_VERSION="4.2.1-1"
+APP_VERSION="4.2.5"
+APP_VERSION_FULL="${APP_VERSION}-1"
+PKG_REL="1"
+
 # https://conda-forge.org/miniforge/
 # https://github.com/conda-forge/miniforge/releases
-MINIFORGE3_VERSION="24.3.0-0"
+MINIFORGE3_VERSION="24.7.1-0"
+
+VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=registry.build.chorus-tre.local}"
 
@@ -16,6 +22,8 @@ OUTPUT="type=${OUTPUT:-docker}"
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
 cp -r ../../core ./core
+trap "rm -rf core" EXIT
+
 docker buildx build \
     --pull \
     -t ${REGISTRY}/${APP_NAME} \
@@ -24,7 +32,7 @@ docker buildx build \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
     --build-arg "APP_VERSION=${APP_VERSION}" \
+    --build-arg "APP_VERSION_FULL=${APP_VERSION_FULL}" \
     --build-arg "MINIFORGE3_VERSION=${MINIFORGE3_VERSION}" \
     --output=$OUTPUT \
     .
-rm -rf core
