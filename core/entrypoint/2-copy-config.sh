a5a4dcb59299005343d383c56eeea7c80daa38bb
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-09-18 18:06:57 +0200

    review

--- core/entrypoint/2-copy-config.sh
+++ core/entrypoint/2-copy-config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-if [ -z "$CHORUS_USER" ]; then
-  echo "CHORUS_USER is not set. Exiting."
-  exit 1
-fi
+set -e
 
 if [ -z "$CONFIG_ARRAY" ]; then
   echo "CONFIG_ARRAY is not set. Exiting."
@@ -14,9 +10,9 @@ fi
 CONFIG_ARRAY=( "$CONFIG_ARRAY" )
 
 #copy all configuration files in $CONFIG_ARRAY in $CHORUS_USER homedir
-for CONFIG in "${CONFIG_ARRAY[@]}"; do
+for CONFIG in ${CONFIG_ARRAY}; do
   echo -n "Copying ${CONFIG} to ${CHORUS_USER} homedir... "
-  cp -r /apps/"${APP_NAME}"/config/"${CONFIG}" ~"${CHORUS_USER}"
-  chown -R "${CHORUS_USER}": ~"${CHORUS_USER}"/"${CONFIG}"
+  eval cp -r /apps/"${APP_NAME}"/config/"${CONFIG}" ~"${CHORUS_USER}"
+  eval chown -R "${CHORUS_USER}": ~"${CHORUS_USER}"/"${CONFIG}"
   echo "Configuration copied for ${CHORUS_USER}"
 done
