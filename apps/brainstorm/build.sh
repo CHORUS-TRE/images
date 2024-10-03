329ff747a95d55fd3714d85fd70f115babc7c91b
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-10-03 15:35:21 +0200

    update to the most recent way of doing it

--- apps/brainstorm/build.sh
+++ apps/brainstorm/build.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+
 set -e
 
-VERSION=0.0.3
 APP_NAME="brainstorm"
-APP_VERSION="latest"
+APP_VERSION="0.0.4"
+PKG_REL="1"
+
+# If the APP_VERSION is bumped, reset the PKG_REL
+# otherwhise, please bump the PKG_REL on any changes.
+VERSION="${APP_VERSION}-${PKG_REL}"
+
 REGISTRY="${REGISTRY:=registry.build.chorus-tre.local}"
+REPOSITORY="${REPOSITORY:=apps}"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
@@ -12,10 +19,11 @@ OUTPUT="type=${OUTPUT:-docker}"
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
 cp -r ../../core ./core
+trap "rm -rf core" EXIT
+
 docker buildx build \
     --pull \
-    -t ${REGISTRY}/${APP_NAME} \
-    -t ${REGISTRY}/${APP_NAME}:${VERSION} \
+    -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
@@ -24,4 +32,3 @@ docker buildx build \
     --build-arg "MAT_UPDATE=6" \
     --output=$OUTPUT \
     .
-rm -rf core
