a896d5e42760d3fe3dddc564853b165eabdd7f70
floriansipp <72794896+floriansipp@users.noreply.github.com>
2024-10-04 13:03:54 +0200

    fix(brainstorm) : chorusify (#31)

--- apps/brainstorm/build.sh
+++ apps/brainstorm/build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-VERSION=0.0.2
+set -e
+
 APP_NAME="brainstorm"
-APP_VERSION="latest"
+APP_VERSION="0.0.4"
+PKG_REL="1"
+
+# If the APP_VERSION is bumped, reset the PKG_REL
+# otherwhise, please bump the PKG_REL on any changes.
+VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=registry.build.chorus-tre.local}"
 REPOSITORY="${REPOSITORY:=apps}"
@@ -12,7 +18,10 @@ OUTPUT="type=${OUTPUT:-docker}"
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
-exec docker buildx build \
+cp -r ../../core ./core
+trap "rm -rf core" EXIT
+
+docker buildx build \
     --pull \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
