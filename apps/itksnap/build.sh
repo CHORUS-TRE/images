864727fe82adeaa92f8d278eeb1d946d0266679e
Yoan Blanc <yoan.blanc@chuv.ch>
2024-12-03 14:15:57 +0000

    feat(apps): add ITK-SNAP (#45)
    * feat(apps): add ITK-SNAP
    
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>
    
    * fixup! feat(apps): add ITK-SNAP
    
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>
    
    * fix: changed registry to harbor
    
    ---------
    
    Signed-off-by: Yoan Blanc <yoan.blanc@chuv.ch>
    Co-authored-by: floriansipp <72794896+floriansipp@users.noreply.github.com>
    Co-authored-by: Nathalie Casati <iDmple@users.noreply.github.com>
    Co-authored-by: Nathalie Casati <nathalie.casati@chuv.ch>

---
+++ apps/itksnap/build.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+APP_NAME="itksnap"
+# See: https://sourceforge.net/projects/itk-snap/files/itk-snap/
+APP_VERSION="4.2.0"
+APP_VERSION_FULL="${APP_VERSION}-20240422"
+PKG_REL="1"
+
+VERSION="${APP_VERSION}-${PKG_REL}"
+
+REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
+
+# Use `registry` to build and push
+OUTPUT="type=${OUTPUT:-docker}"
+
+# Tip: use `BUILDKIT_PROGRESS=plain` to see more.
+
+cp -rp ../../core .
+trap "rm -rf core" EXIT
+
+docker buildx build \
+    --pull \
+    -t ${REGISTRY}/${APP_NAME} \
+    -t ${REGISTRY}/${APP_NAME}:${VERSION} \
+    --label "APP_NAME=${APP_NAME}" \
+    --label "APP_VERSION=${APP_VERSION}" \
+    --build-arg "APP_NAME=${APP_NAME}" \
+    --build-arg "APP_VERSION=${APP_VERSION}" \
+    --build-arg "APP_VERSION_FULL=${APP_VERSION_FULL}" \
+    --output=$OUTPUT \
+    .
