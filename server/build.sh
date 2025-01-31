535372aa6ce7583080192df8ab9eeb7ae710b0ff
Nathalie Casati <iDmple@users.noreply.github.com>
2025-01-31 17:34:06 +0100

    fix(server): fix variable names (#67)

--- server/build.sh
+++ server/build.sh
@@ -44,15 +44,15 @@ else
 fi
 
 # Check if the builder exists
-if ! docker buildx inspect "$BUILDER_NAME" >/dev/null 2>&1; then
-    docker buildx create --name "$BUILDER_NAME" --driver docker-container
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
 fi
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
 exec docker buildx build \
     --pull \
-    --builder docker-container \
+    --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --build-arg "VIRTUALGL_VERSION=${VIRTUALGL_VERSION}" \
     --build-arg "XPRA_VERSION=${XPRA_VERSION}" \
