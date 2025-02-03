CHORUS-TRE/images

commit 8fe1c3f7361645130bdf8a67303665d9bd6ad250

fix(apps): harmonize and docker cache (#70)
* fix(apps): harmonize before introducing docker cache

* fix(server): set -e

* feat(apps): add docker cache, fix version for kiosk, and brainstorm

apps/fsl/build.sh
@@ -12,9 +12,30 @@ VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
 REPOSITORY="${REPOSITORY:=apps}"
+CACHE="${CACHE:=cache}"
+BUILDER_NAME="docker-container"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
+
+if [ "$OUTPUT" = "type=registry" ]; then
+    CACHE_FROM="\
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION} \
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest"
+
+    CACHE_TO="\
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION},mode=max,image-manifest=true \
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest,mode=max,image-manifest=true"
+else
+    mkdir -p /tmp/.buildx-cache  # Ensure cache directory exists
+    CACHE_FROM="--cache-from=type=local,src=/tmp/.buildx-cache"
+    CACHE_TO="--cache-to=type=local,dest=/tmp/.buildx-cache"
+fi
+
+# Check if the builder exists
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
+fi
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
@@ -23,10 +44,13 @@ trap "rm -rf ./core" EXIT
 
 docker buildx build \
     --pull \
+    --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
     --build-arg "APP_VERSION=${APP_VERSION}" \
+    ${CACHE_FROM} \
+    ${CACHE_TO} \
     --output=$OUTPUT \
     .

apps/arx/build.sh
@@ -12,9 +12,30 @@ VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
 REPOSITORY="${REPOSITORY:=apps}"
+CACHE="${CACHE:=cache}"
+BUILDER_NAME="docker-container"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
+
+if [ "$OUTPUT" = "type=registry" ]; then
+    CACHE_FROM="\
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION} \
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest"
+
+    CACHE_TO="\
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION},mode=max,image-manifest=true \
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest,mode=max,image-manifest=true"
+else
+    mkdir -p /tmp/.buildx-cache  # Ensure cache directory exists
+    CACHE_FROM="--cache-from=type=local,src=/tmp/.buildx-cache"
+    CACHE_TO="--cache-to=type=local,dest=/tmp/.buildx-cache"
+fi
+
+# Check if the builder exists
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
+fi
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
@@ -23,10 +44,13 @@ trap "rm -rf ./core" EXIT
 
 docker buildx build \
     --pull \
+    --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
     --build-arg "APP_VERSION=${APP_VERSION}" \
+    ${CACHE_FROM} \
+    ${CACHE_TO} \
     --output=$OUTPUT \
     .

apps/bidsificator/build.sh
@@ -12,21 +12,45 @@ VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
 REPOSITORY="${REPOSITORY:=apps}"
+CACHE="${CACHE:=cache}"
+BUILDER_NAME="docker-container"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
 
+if [ "$OUTPUT" = "type=registry" ]; then
+    CACHE_FROM="\
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION} \
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest"
+
+    CACHE_TO="\
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION},mode=max,image-manifest=true \
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest,mode=max,image-manifest=true"
+else
+    mkdir -p /tmp/.buildx-cache  # Ensure cache directory exists
+    CACHE_FROM="--cache-from=type=local,src=/tmp/.buildx-cache"
+    CACHE_TO="--cache-to=type=local,dest=/tmp/.buildx-cache"
+fi
+
+# Check if the builder exists
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
+fi
+
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
-BUILDKIT_PROGRESS=plain
+
 cp -r ../../core ./core
-trap "rm -rf core" EXIT
+trap "rm -rf ./core" EXIT
 
 docker buildx build \
     --pull \
+    --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
     --build-arg "APP_VERSION=${APP_VERSION}" \
+    ${CACHE_FROM} \
+    ${CACHE_TO} \
     --output=$OUTPUT \
     .

apps/brainstorm/build.sh
@@ -3,7 +3,8 @@
 set -e
 
 APP_NAME="brainstorm"
-APP_VERSION="0.0.4"
+# See: curl -s -I -L "http://neuroimage.usc.edu/bst/getupdate.php?c=UbsM09&src=0&bin=1" | grep -i "content-disposition" | awk -F 'filename=' '{print $2}' | tr -d '\r\n"'
+APP_VERSION="250203"
 PKG_REL="1"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
@@ -12,17 +13,39 @@ VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
 REPOSITORY="${REPOSITORY:=apps}"
+CACHE="${CACHE:=cache}"
+BUILDER_NAME="docker-container"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
 
+if [ "$OUTPUT" = "type=registry" ]; then
+    CACHE_FROM="\
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION} \
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest"
+
+    CACHE_TO="\
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION},mode=max,image-manifest=true \
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest,mode=max,image-manifest=true"
+else
+    mkdir -p /tmp/.buildx-cache  # Ensure cache directory exists
+    CACHE_FROM="--cache-from=type=local,src=/tmp/.buildx-cache"
+    CACHE_TO="--cache-to=type=local,dest=/tmp/.buildx-cache"
+fi
+
+# Check if the builder exists
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
+fi
+
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
 cp -r ../../core ./core
-trap "rm -rf core" EXIT
+trap "rm -rf ./core" EXIT
 
 docker buildx build \
     --pull \
+    --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
@@ -30,5 +53,7 @@ docker buildx build \
     --build-arg "APP_VERSION=${APP_VERSION}" \
     --build-arg "MAT_VERSION=R2023a" \
     --build-arg "MAT_UPDATE=6" \
+    ${CACHE_FROM} \
+    ${CACHE_TO} \
     --output=$OUTPUT \
     .

apps/didata/Dockerfile
@@ -37,11 +37,11 @@ RUN --mount=type=cache,target=/var/cache/apt,sharing=locked \
         libatomic1 \
         xvfb && \
     # Download and install NW.js
-    wget https://dl.nwjs.io/v0.94.0/nwjs-v0.94.0-linux-x64.tar.gz && \
-    tar -xzf nwjs-v0.94.0-linux-x64.tar.gz && \
-    mv nwjs-v0.94.0-linux-x64 /opt/nwjs && \
+    wget https://dl.nwjs.io/v${APP_VERSION}/nwjs-v${APP_VERSION}-linux-x64.tar.gz && \
+    tar -xzf nwjs-v${APP_VERSION}-linux-x64.tar.gz && \
+    mv nwjs-v${APP_VERSION}-linux-x64 /opt/nwjs && \
     ln -s /opt/nwjs/nw /usr/local/bin/nw && \
-    rm nwjs-v0.94.0-linux-x64.tar.gz && \
+    rm nwjs-v${APP_VERSION}-linux-x64.tar.gz && \
     apt-get remove -y --purge wget gpg && \
     apt-get autoremove -y --purge
 

apps/didata/build.sh
@@ -3,18 +3,39 @@
 set -e
 
 APP_NAME="didata"
-APP_VERSION="0.0.1"
+APP_VERSION="0.94.0"
 PKG_REL="1"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
 VERSION="${APP_VERSION}-${PKG_REL}"
 
-REGISTRY="${REGISTRY:=registry.build.chorus-tre.local}"
+REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
 REPOSITORY="${REPOSITORY:=apps}"
+CACHE="${CACHE:=cache}"
+BUILDER_NAME="docker-container"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
+
+if [ "$OUTPUT" = "type=registry" ]; then
+    CACHE_FROM="\
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION} \
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest"
+
+    CACHE_TO="\
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION},mode=max,image-manifest=true \
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest,mode=max,image-manifest=true"
+else
+    mkdir -p /tmp/.buildx-cache  # Ensure cache directory exists
+    CACHE_FROM="--cache-from=type=local,src=/tmp/.buildx-cache"
+    CACHE_TO="--cache-to=type=local,dest=/tmp/.buildx-cache"
+fi
+
+# Check if the builder exists
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
+fi
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
@@ -23,10 +44,13 @@ trap "rm -rf ./core" EXIT
 
 docker buildx build \
     --pull \
+    --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
     --build-arg "APP_VERSION=${APP_VERSION}" \
+    ${CACHE_FROM} \
+    ${CACHE_TO} \
     --output=$OUTPUT \
     .

apps/freesurfer/build.sh
@@ -12,21 +12,45 @@ VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
 REPOSITORY="${REPOSITORY:=apps}"
+CACHE="${CACHE:=cache}"
+BUILDER_NAME="docker-container"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
 
+if [ "$OUTPUT" = "type=registry" ]; then
+    CACHE_FROM="\
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION} \
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest"
+
+    CACHE_TO="\
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION},mode=max,image-manifest=true \
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest,mode=max,image-manifest=true"
+else
+    mkdir -p /tmp/.buildx-cache  # Ensure cache directory exists
+    CACHE_FROM="--cache-from=type=local,src=/tmp/.buildx-cache"
+    CACHE_TO="--cache-to=type=local,dest=/tmp/.buildx-cache"
+fi
+
+# Check if the builder exists
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
+fi
+
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
 cp -r ../../core ./core
-trap "rm -rf core" EXIT
+trap "rm -rf ./core" EXIT
 
 docker buildx build \
     --pull \
+    --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
     --build-arg "APP_VERSION=${APP_VERSION}" \
+    ${CACHE_FROM} \
+    ${CACHE_TO} \
     --output=$OUTPUT \
     .

apps/itksnap/build.sh
@@ -8,26 +8,52 @@ APP_VERSION="4.2.0"
 APP_VERSION_FULL="${APP_VERSION}-20240422"
 PKG_REL="1"
 
+# If the APP_VERSION is bumped, reset the PKG_REL
+# otherwhise, please bump the PKG_REL on any changes.
 VERSION="${APP_VERSION}-${PKG_REL}"
 
 REGISTRY="${REGISTRY:=harbor.build.chorus-tre.local}"
+REPOSITORY="${REPOSITORY:=apps}"
+CACHE="${CACHE:=cache}"
+BUILDER_NAME="docker-container"
 
 # Use `registry` to build and push
 OUTPUT="type=${OUTPUT:-docker}"
 
+if [ "$OUTPUT" = "type=registry" ]; then
+    CACHE_FROM="\
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION} \
+        --cache-from=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest"
+
+    CACHE_TO="\
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:${VERSION},mode=max,image-manifest=true \
+        --cache-to=type=registry,ref=${REGISTRY}/${CACHE}/${APP_NAME}-${CACHE}:latest,mode=max,image-manifest=true"
+else
+    mkdir -p /tmp/.buildx-cache  # Ensure cache directory exists
+    CACHE_FROM="--cache-from=type=local,src=/tmp/.buildx-cache"
+    CACHE_TO="--cache-to=type=local,dest=/tmp/.buildx-cache"
+fi
+
+# Check if the builder exists
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
+fi
+
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
 
-cp -rp ../../core .
-trap "rm -rf core" EXIT
+cp -r ../../core ./core
+trap "rm -rf ./core" EXIT
 
 docker buildx build \
     --pull \
-    -t ${REGISTRY}/${APP_NAME} \
-    -t ${REGISTRY}/${APP_NAME}:${VERSION} \
+    --builder ${BUILDER_NAME} \
+    -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
     --label "APP_NAME=${APP_NAME}" \
     --label "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_NAME=${APP_NAME}" \
     --build-arg "APP_VERSION=${APP_VERSION}" \
     --build-arg "APP_VERSION_FULL=${APP_VERSION_FULL}" \
     --output=$OUTPUT \
+    ${CACHE_FROM} \
+    ${CACHE_TO} \
     .

commit 7d965e2afdf1fa5152ef8ff064fc702feaefea4b

fix(apps): fixes for issues introduced with #70 (#72)
* fix(fsl): new version of fsl

* fix(fsl): new link to installer

apps/fsl/Dockerfile
@@ -14,7 +14,7 @@ RUN --mount=type=cache,target=/var/cache/apt,sharing=locked \
     apt-get install --no-install-recommends -qy \
     ca-certificates curl dc file libgomp1 libquadmath0 locales python3 && \
     locale-gen en_US.UTF-8 en_GB.UTF-8 && \
-    curl -sSL https://fsl.fmrib.ox.ac.uk/fsldownloads/fslinstaller.py -o fslinstaller.py && \
+    curl -sSL https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/releases/fslinstaller.py -o fslinstaller.py && \
     sed -i -E "s/(printmsg\(([^,]+, )?end='(\\\\r)?')/# SILENCE \\1/g" ./fslinstaller.py && \
     python3 ./fslinstaller.py \
         -d /usr/local/fsl \

apps/fsl/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 APP_NAME="fsl"
-APP_VERSION="6.0.7.15"
+APP_VERSION="6.0.7.16"
 PKG_REL="1"
 
 # If the APP_VERSION is bumped, reset the PKG_REL