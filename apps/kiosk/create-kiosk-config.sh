1cd54fff5b12a0b2aa4f4a429774fb31e171fed9
Florian SIPP <florian.sipp@chuv.ch>
2025-02-18 19:22:18 +0100

    feat(kiosk) : generic kiosk app with an url parameter to customize

---
+++ apps/kiosk/create-kiosk-config.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Create index.html
+echo "Starting to create configuration files for the Kiosk"
+echo "Creating index.html"
+cat > /apps/${APP_NAME}/index.html << EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kiosk Browser</title>
+    <script>
+        window.location.href = "${KIOSK_URL}";
+    </script>
+</head>
+<body>
+    <h1>Hello!</h1>
+    <p>Redirecting to the URL provided in the KIOSK_URL environment variable...</p>
+</body>
+</html>
+EOF
+
+# Create package.json
+echo "Creating package.json"
+cat > /apps/${APP_NAME}/package.json << EOF
+{
+    "name": "kiosk",
+    "version": "1.0.0",
+    "main": "index.html",
+    "window": {
+        "kiosk": true,
+        "frame": true,
+        "position": "center",
+        "width": 1024,
+        "height": 768
+    }
+}
+EOF
