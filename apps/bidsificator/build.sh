cfe5b02b36ccd7eaa809cf1b5ab02d8e7eae442c
Florian SIPP <florian.sipp@chuv.ch>
2025-02-20 16:12:41 +0100

    no need for dashes, macos setup issue

--- apps/bidsificator/build.sh
+++ apps/bidsificator/build.sh
@@ -35,8 +35,8 @@ else
 fi
 
 # Check if the builder exists
-if ! docker-buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
-    docker-buildx create --name "${BUILDER_NAME}" --driver docker-container
+if ! docker buildx inspect "${BUILDER_NAME}" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER_NAME}" --driver docker-container
 fi
 
 # Tip: use `BUILDKIT_PROGRESS=plain` to see more.
@@ -44,7 +44,7 @@ fi
 cp -r ../../core ./core
 trap "rm -rf ./core" EXIT
 
-docker-buildx build \
+docker buildx build \
     --pull \
     --builder ${BUILDER_NAME} \
     -t ${REGISTRY}/${REPOSITORY}/${APP_NAME}:${VERSION} \
