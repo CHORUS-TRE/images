0f92ba5a2eb08361947d8fbab2487fe1271aa48d
Florian SIPP <florian.sipp@chuv.ch>
2025-04-02 17:30:16 +0200

    update fsl version

--- apps/fsl/build.sh
+++ apps/fsl/build.sh
@@ -3,8 +3,8 @@
 set -e
 
 APP_NAME="fsl"
-APP_VERSION="6.0.7.16"
-PKG_REL="2"
+APP_VERSION="6.0.7.17"
+PKG_REL="1"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
