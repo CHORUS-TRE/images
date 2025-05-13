3236e90c1165046d64f23e9eb935cd24a1810dfd
Nathalie Casati <nathalie.casati@chuv.ch>
2025-05-13 11:58:26 +0000

    fix(kiosk): change window size

--- apps/kiosk/start-kiosk.sh
+++ apps/kiosk/start-kiosk.sh
@@ -6,7 +6,7 @@ export GOOGLE_API_KEY="no"
 export GOOGLE_DEFAULT_CLIENT_ID="no"
 export GOOGLE_DEFAULT_CLIENT_SECRET="no"
 
-/usr/local/bin/chrome-linux/chrome --noerrdialogs --disable-infobars --app=${KIOSK_URL} --window-size=1200,800 &
+/usr/local/bin/chrome-linux/chrome --noerrdialogs --disable-infobars --app=${KIOSK_URL} --window-size=1200,700 &
 
 CHROMIUM_PID=$!
 
