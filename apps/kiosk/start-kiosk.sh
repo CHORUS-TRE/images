8ddffbf9e61f9ffb075e7dd292d6d9cb419eea3c
Nathalie Casati <iDmple@users.noreply.github.com>
2025-05-14 13:44:58 +0200

    fix(kiosk): rename window, only for future ref (#104)
    * fix(kiosk): change window size
    
    * fix(kiosk): rename window, only for future ref
    
    * fix(kiosk): rename window, only for future ref
    
    * fix(kiosk): rename window, only for future ref

--- apps/kiosk/start-kiosk.sh
+++ apps/kiosk/start-kiosk.sh
@@ -6,13 +6,22 @@ export GOOGLE_API_KEY="no"
 export GOOGLE_DEFAULT_CLIENT_ID="no"
 export GOOGLE_DEFAULT_CLIENT_SECRET="no"
 
-/usr/local/bin/chrome-linux/chrome --noerrdialogs --disable-infobars --app=${KIOSK_URL} --window-size=1200,700 &
+/usr/local/bin/chrome-linux/chrome --noerrdialogs --disable-infobars --disable-session-crashed-bubble --app=${KIOSK_URL} --window-size=1200,700 &
 
 CHROMIUM_PID=$!
 
-sleep 2
+echo "Waiting for ${KIOSK_URL} Chromium window to appear..."
+WIN_ID=""
 
-WIN_ID=$(wmctrl -l | grep "$WEB_UI" | awk '{print $1}')
-wmctrl -i -r $WIN_ID -T "$APP_NAME"
+while [ -z "$WIN_ID" ]; do
+    WIN_ID=$(wmctrl -l | grep "$APP_NAME" | awk '{print $1}' | head -n1)
+    sleep 1
+done
+
+echo "Updating window title..."
+while kill -0 $CHROMIUM_PID 2>/dev/null; do
+    wmctrl -i -r $WIN_ID -T "$APP_NAME"
+    sleep 1
+done
 
 wait $CHROMIUM_PID
