372c34d47ad34b0977885eae053390930abd8ca0
Nathalie Casati <nathalie.casati@chuv.ch>
2025-05-16 10:55:36 +0000

    fix(kiosk): optmize qiosk

--- apps/kiosk/start-qiosk.sh
+++ apps/kiosk/start-qiosk.sh
@@ -2,11 +2,25 @@
 
 set -e
 
-# needed as long as we don't have a GPU
-export QT_QUICK_BACKEND=software
-export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu"
+# env vars below needed as long as we don't have a GPU
 
-qiosk -m maximized --display-scroll-bars --display-navbar --underlay-navbar --navbar-enable-buttons back,forward,reload,home $KIOSK_URL
+# using this makes it prettier
+# export QT_QUICK_BACKEND=software
+
+# using this makes it slower
+# export QT_OPENGL=software
+
+# using this gives info
+export QSG_INFO=1
+
+# unsure this makes a difference
+export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --enable-software-rasterizer --disable-features=Vulkan"
+
+# unsure these two make a difference
+export QT_FONT_DPI=96
+export QT_SCALE_FACTOR=1
+
+qiosk -m automaticvisibility --display-scroll-bars $KIOSK_URL
 QIOSK_PID=$!
 
 wait $QIOSK_PID
