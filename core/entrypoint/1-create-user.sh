66984b2b49f11d0216b6f28196cdbdfe2ad18c33
floriansipp <72794896+floriansipp@users.noreply.github.com>
2025-08-15 14:14:26 +0200

    fix(core) : chown user home to assign correct permissions + bump one app to test (#131)
    * chown user home to assign correct permissions + bump one app to test
    
    * new plan
    
    * nice
    
    * nicer

--- core/entrypoint/1-create-user.sh
+++ core/entrypoint/1-create-user.sh
@@ -6,7 +6,13 @@ if grep -E "^$CHORUS_USER" /etc/passwd >/dev/null; then
   echo "$CHORUS_USER already exists."
   exit 0
 else
-  if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid 1001; then
+
+if [ -d "/home/$CHORUS_USER" ]; then
+  echo "home directory already exists, updating permissions"
+  chown 1001:1001 /home/$CHORUS_USER
+fi
+
+if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid 1001 ; then
     echo "done."
   else
     echo "failed."
