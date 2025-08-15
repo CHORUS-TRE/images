cfbbbbc4c029472aee06194deac416d212d0a65d
Florian SIPP <florian.sipp@chuv.ch>
2025-08-15 13:52:10 +0200

    chown user home to assign correct permissions + bump one app to test

--- core/entrypoint/1-create-user.sh
+++ core/entrypoint/1-create-user.sh
@@ -6,7 +6,7 @@ if grep -E "^$CHORUS_USER" /etc/passwd >/dev/null; then
   echo "$CHORUS_USER already exists."
   exit 0
 else
-  if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid 1001; then
+  if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid 1001 && chown "$CHORUS_USER":"$CHORUS_USER" "/home/$CHORUS_USER"; then
     echo "done."
   else
     echo "failed."
