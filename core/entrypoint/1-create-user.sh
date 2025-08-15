36dd18a08362ea4904b4cee1500d79e55906ec45
Nathalie Casati <nathalie.casati@chuv.ch>
2025-08-15 14:29:06 +0200

    fix(core): add skel

--- core/entrypoint/1-create-user.sh
+++ core/entrypoint/1-create-user.sh
@@ -6,16 +6,23 @@ if grep -E "^$CHORUS_USER" /etc/passwd >/dev/null; then
   echo "$CHORUS_USER already exists."
   exit 0
 else
-
-if [ -d "/home/$CHORUS_USER" ]; then
-  echo "home directory already exists, updating permissions"
-  chown 1001:1001 /home/$CHORUS_USER
-fi
-
-if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid 1001 ; then
-    echo "done."
+  if [ -d "/home/$CHORUS_USER" ]; then
+    echo "home directory already exists, updating permissions"
+    chown 1001:1001 /home/$CHORUS_USER
+    if useradd --no-create-home --shell /bin/bash "$CHORUS_USER" --uid 1001; then
+      echo "copying skeleton files..."
+      cp -r /etc/skel/. "/home/$CHORUS_USER/"
+      echo "done."
+    else
+      echo "failed."
+      exit 1
+    fi
   else
-    echo "failed."
-    exit 1
+    if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid 1001; then
+      echo "done."
+    else
+      echo "failed."
+      exit 1
+    fi
   fi
 fi
