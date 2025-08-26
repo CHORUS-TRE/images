741eb7a0a85d75c7b28c2cc2aca1351c344ceddd
Nathalie Casati <iDmple@users.noreply.github.com>
2025-08-26 14:24:29 +0200

    feat(core): added user/group support (#140)
    * feat(core): added user/group support
    
    * bump sciterminal to test core updates
    
    ---------
    
    Co-authored-by: Florian SIPP <florian.sipp@chuv.ch>

--- core/entrypoint/1-create-user.sh
+++ core/entrypoint/1-create-user.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-echo -n "Creating user $CHORUS_USER... "
+echo -n "Creating group $CHORUS_GROUP with GID $CHORUS_GID... "
+if ! groupadd -g $CHORUS_GID $CHORUS_GROUP; then
+  echo "failed: group $CHORUS_GROUP or GID $CHORUS_GID already exists"
+  exit 1
+fi
 
-if grep -E "^$CHORUS_USER" /etc/passwd >/dev/null; then
-  echo "$CHORUS_USER already exists."
-  exit 0
-else
-  if [ -d "/home/$CHORUS_USER" ]; then
-    if useradd --no-create-home --shell /bin/bash "$CHORUS_USER" --uid 1001; then
-      cp -r /etc/skel/. "/home/$CHORUS_USER/"
-      find "/home/$CHORUS_USER" -not -path "/home/$CHORUS_USER/workspace-data/*" -exec chown $CHORUS_USER:$CHORUS_USER {} \;
-      echo "done and updated permissions."
-    else
-      echo "failed."
-      exit 1
-    fi
+echo -n "Creating user $CHORUS_USER with UID $CHORUS_UID... "
+if getent passwd "$CHORUS_USER" >/dev/null; then
+  echo "failed: $CHORUS_USER already exists."
+  exit 1
+fi
+
+if [ -d "/home/$CHORUS_USER" ]; then
+  if useradd --no-create-home --shell /bin/bash "$CHORUS_USER" --uid "$CHORUS_UID" --gid "$CHORUS_GID"; then
+   cp -a /etc/skel/. "/home/$CHORUS_USER/"
+    find "/home/$CHORUS_USER" -path "/home/$CHORUS_USER/workspace-data" -prune -o -exec chown "$CHORUS_USER:$CHORUS_GID" {} +
+    chown "$CHORUS_USER:$CHORUS_GID" "$HOME_DIR/workspace-data"
+    echo "done and updated permissions."
   else
-    if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid 1001; then
-      echo "done."
-    else
-      echo "failed."
-      exit 1
-    fi
+    echo "failed: could not add user (without homedir)."
+    exit 1
+  fi
+else
+  if useradd --create-home --shell /bin/bash "$CHORUS_USER" --uid "$CHORUS_UID" --gid "$CHORUS_GID"; then
+    echo "done."
+  else
+    echo "failed: could not add user (with homedir)."
+    exit 1
   fi
 fi
