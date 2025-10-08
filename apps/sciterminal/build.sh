158557e12ee70f3028eefd4ce9e54f434e5cea3a
Florian SIPP <florian.sipp@chuv.ch>
2025-10-08 14:48:26 +0200

    chore(apps) : bump versions to integrate modification to core scripts

--- apps/sciterminal/build.sh
+++ apps/sciterminal/build.sh
@@ -4,7 +4,7 @@ set -e
 
 APP_NAME="sciterminal"
 APP_VERSION="20250826"
-PKG_REL="2"
+PKG_REL="3"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
