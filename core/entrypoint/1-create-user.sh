da28c5873b73bc8d4a67d2ed40fc7c0908308ab7
floriansipp <72794896+floriansipp@users.noreply.github.com>
2025-10-08 12:04:05 +0200

    fix(core) : fix path permissions for storage and bump filemanager to test (#150)

--- core/entrypoint/1-create-user.sh
+++ core/entrypoint/1-create-user.sh
@@ -15,8 +15,10 @@ fi
 if [ -d "/home/$CHORUS_USER" ]; then
   if useradd --no-create-home --shell /bin/bash "$CHORUS_USER" --uid "$CHORUS_UID" --gid "$CHORUS_GID"; then
    cp -a /etc/skel/. "/home/$CHORUS_USER/"
-    find "/home/$CHORUS_USER" -path "/home/$CHORUS_USER/workspace-data" -prune -o -exec chown "$CHORUS_USER:$CHORUS_GID" {} +
-    chown "$CHORUS_USER:$CHORUS_GID" "/home/$CHORUS_USER/workspace-data"
+    find "/home/$CHORUS_USER" -path "/home/$CHORUS_USER/workspace-scratch" -prune -o -exec chown "$CHORUS_USER:$CHORUS_GID" {} +
+    find "/home/$CHORUS_USER" -path "/home/$CHORUS_USER/workspace-archive" -prune -o -exec chown "$CHORUS_USER:$CHORUS_GID" {} +
+    chown "$CHORUS_USER:$CHORUS_GID" "/home/$CHORUS_USER/workspace-scratch"
+    chown "$CHORUS_USER:$CHORUS_GID" "/home/$CHORUS_USER/workspace-archive"
     echo "done and updated permissions."
   else
     echo "failed: could not add user (without homedir)."
