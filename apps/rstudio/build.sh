c64dc95dd7c212f946c653fcf8ec0f15e73628df
floriansipp <72794896+floriansipp@users.noreply.github.com>
2025-10-13 15:11:24 +0200

    fix(core) : change symlink ownership without dereferencing (#156)
    Bump everything

--- apps/rstudio/build.sh
+++ apps/rstudio/build.sh
@@ -4,7 +4,7 @@ set -e
 
 APP_NAME="rstudio"
 APP_VERSION=2025.05.1
-PKG_REL="4"
+PKG_REL="5"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
