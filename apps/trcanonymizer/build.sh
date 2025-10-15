2e1d5da77f2520a011a82fc30b8c4f0b4e41590a
Florian SIPP <florian.sipp@chuv.ch>
2025-10-15 09:11:56 +0200

    chore(apps) : rerun build apps

--- apps/trcanonymizer/build.sh
+++ apps/trcanonymizer/build.sh
@@ -4,7 +4,7 @@ set -e
 
 APP_NAME="trcanonymizer"
 APP_VERSION="1.1.0"
-PKG_REL="12"
+PKG_REL="13"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
