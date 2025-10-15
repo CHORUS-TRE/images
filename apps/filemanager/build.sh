87bc7f87f79c6bcce19e13d766bee6a45747a49d
floriansipp <72794896+floriansipp@users.noreply.github.com>
2025-10-15 09:14:09 +0200

    chore(apps) : rerun build apps (#158)

--- apps/filemanager/build.sh
+++ apps/filemanager/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 APP_NAME="filemanager"
-APP_VERSION="20251013"
+APP_VERSION="20251014"
 PKG_REL="1"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
--- apps/fsl/build.sh
+++ apps/fsl/build.sh
@@ -4,7 +4,7 @@ set -e
 
 APP_NAME="fsl"
 APP_VERSION="6.0.7.17"
-PKG_REL="10"
+PKG_REL="11"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
