8dacc9d120c1576e63af84f77d097abc8c89e10f
Jonathan Haab <jonathan.haab@chuv.ch>
2025-10-22 10:16:06 +0200

    change pkg rel

--- apps/bidsificator/build.sh
+++ apps/bidsificator/build.sh
@@ -4,7 +4,7 @@ set -e
 
 APP_NAME="bidsificator"
 APP_VERSION="1.10.0"
-PKG_REL="7"
+PKG_REL="4"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
