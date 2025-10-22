03788668f5268813d32ebe5ad58ae2228d025adc
Jonathan Haab <jonathan.haab@chuv.ch>
2025-10-22 12:00:22 +0200

    bump

--- apps/trcanonymizer/build.sh
+++ apps/trcanonymizer/build.sh
@@ -4,7 +4,7 @@ set -e
 
 APP_NAME="trcanonymizer"
 APP_VERSION="1.1.0"
-PKG_REL="15"
+PKG_REL="17"
 
 # If the APP_VERSION is bumped, reset the PKG_REL
 # otherwhise, please bump the PKG_REL on any changes.
