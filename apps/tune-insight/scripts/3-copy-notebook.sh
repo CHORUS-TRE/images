56b7a967189bc927cd0a5a3b7e331526f1e10337
Jonathan Haab <71326949+johaab@users.noreply.github.com>
2026-02-03 10:21:17 +0100

    Feat(apps): add tune insight wheels and latest notebooks (#201)
    * add ti wheels and data
    
    * add tuneinsight wheels and latest notebooks

--- apps/tune-insight/scripts/3-copy-notebook.sh
+++ apps/tune-insight/scripts/3-copy-notebook.sh
@@ -13,7 +13,7 @@ NOTEBOOK_ARRAY=( "$NOTEBOOK_ARRAY" )
 # if it does not already exist
 for NOTEBOOK in ${NOTEBOOK_ARRAY}; do
   echo -n "Copying ${NOTEBOOK} to ${CHORUS_USER} ${PERSISTED_FOLDER}... "
-  eval cp -nr /apps/"${APP_NAME}"/notebook/"${NOTEBOOK}" ~"${CHORUS_USER}"/"${PERSISTED_FOLDER}"
+  eval cp -r --update=none /apps/"${APP_NAME}"/notebook/"${NOTEBOOK}" ~"${CHORUS_USER}"/"${PERSISTED_FOLDER}"
   eval chown -R "${CHORUS_USER}": ~"${CHORUS_USER}"/"${PERSISTED_FOLDER}"/"${NOTEBOOK}"
   echo "Notebook copied for ${CHORUS_USER}"
 done
