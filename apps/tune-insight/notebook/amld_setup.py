4079d167c38ae87441bd54cfcee7065e6be8da08
Jonathan Haab <jonathan.haab@chuv.ch>
2026-02-11 18:35:56 +0100

    add latest notebooks and update ti-models

--- apps/tune-insight/notebook/amld_setup.py
+++ apps/tune-insight/notebook/amld_setup.py
@@ -5,4 +5,4 @@ import os
 os.environ["NODE_URL"] = "https://amld-workshop-node1.demo.tuneinsight.net"
 os.environ["OIDC_URL"] = "https://auth.tuneinsight.com/auth/"
 os.environ["OIDC_CLIENT_ID"] = "amld1-front-8atme9aty9"
-os.environ["TI_VERIFY_SSL"] = "False"
+os.environ["TI_VERIFY_SSL"] = "True"
